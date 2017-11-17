a77b25377f4fe61f4c4e0aad2cd24b086d6bfa9d
Johnathan Ritucci <john.l.ritucci@gmail.com>
2017-11-17 00:47:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,9 @@ function sayHiToGrandma(string) {
   if (string === lowercase){
     return "I can't hear you!"}
   else if (string === uppercase) {
-    return "YES INDEED!"
+    return "YES INDEED!"}
+  else if (string === "I love you, Grandma."){
+    return "I love you, too."
   }
+  
 }
